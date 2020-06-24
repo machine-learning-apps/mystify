2938d5c263e97b0033351e63b5c05ac87f4103ff
Michał Jastrzębski <inc0@github.com>
2020-06-24 12:13:03 -0700

    few comments

--- amalthea/markdown_contents_manager.py
+++ amalthea/markdown_contents_manager.py
@@ -3,5 +3,11 @@ from notebook.services.contents.largefilemanager import LargeFileManager
 
 class MarkdownContentsManager(LargeFileManager):
     def save(self, model, path=''):
-        # this is where we put formatting code
+        self.log.debug("Converting into amalthea markdown")
+        # this is where we put model to markdown code
+        # maybe we can use pre_save hooks (https://github.com/jupyter/notebook/blob/master/notebook/services/contents/manager.py#L86)
         return super(MarkdownContentsManager, self).save(model, path)
+
+    def get(self, path, content=True, type=None, format=None):
+        # This is markdown to model
+        return super(MarkdownContentsManager, self).get(path, content, type, format)
