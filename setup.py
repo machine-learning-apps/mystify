e03abf38830a97b7502b64dde6bf6ab49b38c2c6
David Aronchick <aronchick@gmail.com>
2020-06-29 20:54:43 +0000

    rebasing to main

--- setup.py
+++ setup.py
@@ -25,7 +25,6 @@ setup(
     author_email="NYI",
     url="https://github.com/machine-learning-apps/mystify",
     license="MIT License",
-<<<<<<< HEAD
     keywords=[
         "Jupyter",
         "Machine Learning",
@@ -47,27 +46,4 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.8",
     ],
-=======
-    keywords=["Jupyter", "Machine Learning", "Mystify"],  # Keywords that define your package best
-    install_requires=[
-        "wheel",
-        'jupyter',
-        'myst-parser'
-    ],
-    packages=["mystify"],
-    include_package_data=True,
-    package_data={"": extra_files},
-    zip_safe=False,
-    classifiers=[
-        "Development Status :: 3 - Alpha",  # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-        "Intended Audience :: Developers",  # Define that your audience are developers
-        "Topic :: Software Development :: Build Tools",
-        "License :: OSI Approved :: MIT License",  # Again, pick a license
-        "Programming Language :: Python :: 3",  # Specify which pyhton versions that you want to support
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.8",
-    ],
->>>>>>> 0e58639fafb6b5f48ffb0dea708e2d98df51c6e3
 )
