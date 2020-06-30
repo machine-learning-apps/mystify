1b584ffdd94b8574dc06a2bf0357a178c7b1db90
Michał Jastrzębski <inc0@github.com>
2020-06-30 16:06:21 -0700

    load test passing

--- mystify/convert.py
+++ mystify/convert.py
@@ -8,23 +8,38 @@ def to_myst(model):
     pass
 
 
+def _parse_code_output(cell_section):
+    out = {}
+    out['output_type'] = cell_section.info.split(" ")[1]
+    if out['output_type'] == 'stream':
+        out['text'] = cell_section.content.splitlines(True)
+        out['name'] = 'stdout'
+    return out
+
+
 def _parse_cell(cell):
     cell_meta = yaml.load(cell[0].content)
+    if not "metadata" in cell_meta:
+        cell_meta["metadata"] = {}
     for cell_section in cell[1:]:
         if cell_section.info == "{source}":
-            cell_meta["source"] = re.findall("\n", cell_section.content)
-        
-        from IPython import embed; embed()
+            cell_meta["source"] = cell_section.content.splitlines(True)
+            cell_meta["source"][-1] = cell_meta["source"][-1].strip()
+        if cell_section.info.startswith("{output}"):
+            out = _parse_code_output(cell_section)
+            if 'outputs' not in cell_meta:
+                cell_meta['outputs'] = []
+            cell_meta['outputs'].append(out)
     return cell_meta
 
 
 def _split_sections(tokens):
-    sections = {"cells": []}
     append_to_cell = None
     for token in tokens:
         if token.type == 'fence' and token.info == '{metadata}':
             # Metadata yaml block
-            sections['metadata'] = yaml.load(token.content)
+            sections = yaml.load(token.content)
+            sections["cells"] = []
         if append_to_cell is not None:
             append_to_cell.append(token)
         if token.type == 'myst_line_comment' and token.content == 'cell':
@@ -39,4 +54,5 @@ def to_model(myst):
     md = default_parser("docutils")
     tokens = md.parse(myst)
     sections = _split_sections(tokens)
+    return sections
 
